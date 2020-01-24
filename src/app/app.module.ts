b3b6b149aa8b617cfb6cea0d00cdfd97011a98a4
Tecnico2011 <garciafebresd@gmail.com>
2020-01-23 23:56:17 -0300

    auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { FormsModule } from "@angular/forms";
 
@@ -21,6 +22,7 @@ import { LoginComponent } from './pages/login/login.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
