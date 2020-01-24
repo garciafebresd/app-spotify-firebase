4d96ada792fa747e81ee66e4e2226edd5f993fb8
Tecnico2011 <garciafebresd@gmail.com>
2020-01-24 00:05:29 -0300

    service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserModel } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private url = 'https://identitytoolkit.googleapis.com/v1/accounts';
+  private API_KEY = 'AIzaSyDQDoNn3swVlrNokqOL-Q8EcfevvBiwQxg';
+
+
+  constructor(private http: HttpClient) { }
+
+  login(usuario: UserModel) {
+
+  }
+
+  logout() {
+
+  }
+
+  register(usuario: UserModel) {
+
+  }
+
+  //CREAR USUARIO
+  //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+  //LOGIN USUARIO
+  //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+
 }
